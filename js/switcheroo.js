532c8cb28a6dc4bc4b3bf1e0542c9097d440ef0e
kriss <kriss@xixs.com>
2020-06-13 18:09:01 +0100

    .

--- js/switcheroo.js
+++ js/switcheroo.js
@@ -7,10 +7,20 @@ var switcheroo=exports;
 let jquery=require("jquery")
 let $=jquery
 
+var stringify = require('json-stable-stringify');
+
 let dflat=require("d-portal/dflat/js/dflat.js")
 
 
 switcheroo.got_files = false;
+
+switcheroo.act_count=0
+switcheroo.org_count=0
+
+switcheroo.data      = false;
+switcheroo.data_xml  = false;
+switcheroo.data_csv  = false;
+switcheroo.data_html = false;
 
 
 // call at page load
@@ -29,7 +39,7 @@ switcheroo.start = function()
 
 
 // get the file data
-switcheroo.process=function()
+switcheroo.load=function()
 {
 	if( switcheroo.got_files )
 	{
@@ -38,13 +48,96 @@ switcheroo.process=function()
 		reader.onload = (function(theFile) {
 			return function(e) {
 
-				console.log( e.target.result )
+				switcheroo.parse( e.target.result )
 
 			};
 		})(f);
 		reader.readAsText(f);
 	}
 	switcheroo.got_files=false
+}
+
+// get the file data
+switcheroo.parse=function(data)
+{
+	let json=false
+
+	// json
+	try{
+		json=JSON.parse(data)
+	}catch(e){}
+	
+	// xml
+	if(!json)
+	{
+		try{
+			json=dflat.xml_to_xson(data)
+		}catch(e){}
+	}
+
+	// csv
+	if(!json)
+	{
+		try{
+			json=dflat.xsv_to_xson(data)
+		}catch(e){}
+	}
+	
+	switcheroo.data=json || {}
+	
+	switcheroo.convert()
+	
+	switcheroo.fixup_download()
+
+}
+
+// convert the global data
+switcheroo.convert=function()
+{
+	let x=switcheroo.data
+	
+	if( x && x["/iati-activities/iati-activity"] )
+	{
+		switcheroo.act_count=x["/iati-activities/iati-activity"].length
+	}
+	else
+	{
+		switcheroo.act_count=0
+	}
+
+	if( x && x["/iati-organisations/iati-organisation"] )
+	{
+		switcheroo.org_count=x["/iati-organisations/iati-organisation"].length
+	}
+	else
+	{
+		switcheroo.org_count=0
+	}
+	
+	console.log("acts : "+switcheroo.act_count)
+	console.log("orgs : "+switcheroo.org_count)
+	
+	if( switcheroo.act_count + switcheroo.org_count > 0 ) // we have some data so convert it
+	{
+		switcheroo.data_xml=dflat.xson_to_xml(x)
+		if( switcheroo.org_count > 0 )
+		{
+			switcheroo.data_csv=dflat.xson_to_xsv(x,"/iati-organisations/iati-organisation",{"/iati-organisations/iati-organisation":true})
+		}
+		else
+		{
+			switcheroo.data_csv=dflat.xson_to_xsv(x,"/iati-activities/iati-activity",{"/iati-activities/iati-activity":true})
+		}
+		switcheroo.data_html=dflat.xson_to_html(x)
+	}
+	else
+	{
+		switcheroo.data = false;
+		switcheroo.data_xml  = false;
+		switcheroo.data_csv  = false;
+		switcheroo.data_html = false;
+	}
+
 }
 
 
@@ -65,15 +158,48 @@ switcheroo.fixup=function()
 		$box.removeClass('is-dragover');
 	})
 	.on('drop', function(e) {
-		console.log(e)
 		switcheroo.got_files = e.originalEvent.dataTransfer.files;
-		switcheroo.process()
+		switcheroo.load()
 	});
 
 	$("#switcheroo_file").on('change', function(e)
 	{
 		switcheroo.got_files = e.target.files;
-		switcheroo.process()
+		switcheroo.load()
 	})
 
+	switcheroo.fixup_download()
 }
+
+switcheroo.fixup_download=function()
+{
+
+	if(switcheroo.data)
+	{
+		$("#switcheroo_html").show()
+		$("#switcheroo_html").attr('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(switcheroo.data_html) )
+		$("#switcheroo_html").attr('download', "switcheroo.html")
+
+		$("#switcheroo_xml").show()
+		$("#switcheroo_xml").attr('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(switcheroo.data_xml))
+		$("#switcheroo_xml").attr('download', "switcheroo.xml")
+
+		$("#switcheroo_json").show()
+		$("#switcheroo_json").attr('href', 'data:application/json;charset=utf-8,' + encodeURIComponent( stringify(switcheroo.data,{space:" "}) ))
+		$("#switcheroo_json").attr('download', "switcheroo.json")
+
+		$("#switcheroo_csv").show()
+		$("#switcheroo_csv").attr('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent( switcheroo.data_csv ))
+		$("#switcheroo_csv").attr('download', "switcheroo.csv")
+
+	}
+	else
+	{
+		$("#switcheroo_html").hide()
+		$("#switcheroo_xml").hide()
+		$("#switcheroo_json").hide()
+		$("#switcheroo_csv").hide()
+	}
+
+}
+
